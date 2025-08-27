d23b8b12552df4587a2f7fd71007ca2ae591842f
rajes <rajes@DESKTOP-ML3FKTI>
2025-08-27 07:59:25 +0530

    Add project files.

---
+++ IOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kafka
+{
+    internal interface IOrder
+    {
+        string Name { get; }
+        void OrderType();
+
+    }
+}
